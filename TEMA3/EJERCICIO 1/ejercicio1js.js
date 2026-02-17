31f1a0d9755e1ab8b44530e1278f51aae4ae06ff
JosemaMR <josemamr2005@gmail.com>
2026-02-17 20:01:34 +0100

    Add functions to change and clear paragraph text

--- TEMA3/EJERCICIO 1/ejercicio1js.js
+++ TEMA3/EJERCICIO 1/ejercicio1js.js
@@ -1,1 +1,10 @@
+// Function to change paragraph text
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+
+// Function to clear paragraph text
+function clearParagraph() {
+    document.getElementById("demo").innerHTML = "";
+}
 
