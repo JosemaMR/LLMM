d77eb6a4ad5fb5909859d94f7858a33a76c2ab92
JosemaMR <josemamr2005@gmail.com>
2026-02-17 20:08:29 +0100

    Add functions to change images and styles

--- TEMA3/EJERCICIO 3/ejercicio3js.js
+++ TEMA3/EJERCICIO 3/ejercicio3js.js
@@ -1,1 +1,20 @@
 
+function cambiaCoche(nombre) {
+    document.getElementById('imagenCoche').src = nombre;
+}
+
+
+function cambiaSpiderman(nombre) {
+    document.getElementById('imagenSpiderman').src = nombre;
+}
+
+function activarEstilo(estilo) {
+    if(estilo === 'estilo1') {
+        document.getElementById('estilo1').disabled = false;
+        document.getElementById('estilo2').disabled = true;
+    } else {
+        document.getElementById('estilo1').disabled = true;
+        document.getElementById('estilo2').disabled = false;
+    }
+}
+
