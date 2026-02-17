262fe4e24b6d9f84872c308843108ee47e84c36b
JosemaMR <josemamr2005@gmail.com>
2026-02-17 20:05:24 +0100

    Update ejercicio1js.js

--- TEMA3/EJERCICIO 1/ejercicio1js.js
+++ TEMA3/EJERCICIO 1/ejercicio1js.js
@@ -1,9 +1,8 @@
-// Function to change paragraph text
+
 function myFunction() {
     document.getElementById("demo").innerHTML = "Paragraph changed.";
 }
 
-// Function to clear paragraph text
 function clearParagraph() {
     document.getElementById("demo").innerHTML = "";
 }
