1d1500698c4af3cdf76e1cd7e944197509cf6419
JosemaMR <josemamr2005@gmail.com>
2026-03-17 19:31:34 +0100

    Add image carousel functionality in app.js

---
+++ TEMA3/EJERCICIO 7/app.js
@@ -0,0 +1,24 @@
+const pictures = [
+  "contBcg-0",
+  "contBcg-1",
+  "contBcg-2",
+  "contBcg-3",
+  "contBcg-4"
+];
+
+let counter = 0;
+const imageDIV = document.getElementById('img-container');
+
+function changeImage(direction) {
+  counter += direction;
+
+  if (counter < 0) {
+    counter = pictures.length - 1;
+  }
+
+  if (counter > pictures.length - 1) {
+    counter = 0;
+  }
+
+  imageDIV.style.backgroundImage = `url('./img/${pictures[counter]}.jpeg')`;
+}
